5b7bec7a31bd6d5005d0c2e0dd50b42577fe02a8
obfromevi <obfromevi@gmail.com>
2025-09-09 10:19:02 +0200

    LUT-30688 : update pom.xml and add liquibase tags to SQL files

--- src/sql/plugins/elasticdata-forms/plugin/create_db_elasticdata-forms.sql
+++ src/sql/plugins/elasticdata-forms/plugin/create_db_elasticdata-forms.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+-- changeset elasticdata-forms:create_db_elasticdata-forms.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 
 --
 -- Structure for table elasticdata_forms_optionalquestionresponse
@@ -6,7 +9,7 @@
 DROP TABLE IF EXISTS elasticdata_forms_optionalquestionresponse;
 CREATE TABLE elasticdata_forms_optionalquestionresponse (
 id_optional_question_indexation int AUTO_INCREMENT,
-id_form int default '0' NOT NULL,
-id_question int default '0' NOT NULL,
+id_form int default 0 NOT NULL,
+id_question int default 0 NOT NULL,
 PRIMARY KEY (id_optional_question_indexation)
 );
