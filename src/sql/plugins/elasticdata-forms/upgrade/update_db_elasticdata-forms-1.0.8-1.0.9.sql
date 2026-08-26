a1539ba3bc7d837a506fa585e049aded345439b6
yahiaoui <rafik.yahiaoui@paris.fr>
2026-08-26 09:04:32 +0200

    lutece:liquibase-sql

--- src/sql/plugins/elasticdata-forms/upgrade/update_db_elasticdata-forms-1.0.8-1.0.9.sql
+++ src/sql/plugins/elasticdata-forms/upgrade/update_db_elasticdata-forms-1.0.8-1.0.9.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+-- changeset elasticdata-forms:update_db_elasticdata-forms-1.0.8-1.0.9.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 DROP TABLE IF EXISTS workflowstatus_forms_optionalstatus;
 CREATE TABLE workflowstatus_forms_optionalstatus (
 id_optional_status_indexation int AUTO_INCREMENT,
